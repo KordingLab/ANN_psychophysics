decabb0e6fce91f1d9bcfaaf35cdab148e8c3993
Ryan Guan <guan.ryan@gmail.com>
2019-02-10 02:57:18 -0500

    width lines and width ellipses added

--- from_lines/data_generators.py
+++ from_lines/data_generators.py
@@ -5,47 +5,61 @@ from scipy.ndimage import rotate
 from skimage.draw import (line_aa, line, bezier_curve,polygon_perimeter, polygon,
                           ellipse, ellipse_perimeter)
 import matplotlib.pyplot as plt
-
+import math
 
 
 ### define methods in torch to create the input data
 
-def batch_inputs(filts, batchsize = 64):
-    """Wraps the construction of the oriented lines, batches them, and returns pytorch tensors"""
-    inputs,targets = list(zip(*[[torch.tensor(im, dtype = torch.float) for im in single_black_line(filts)]
-                                       for _ in range(batchsize)]))
 
-    inputs = torch.stack(inputs)
-    targets = torch.stack(targets)
-    assert inputs.size()[0] == batchsize
-
-    return inputs,targets
+def generate_shrunken_array_based_on_width(width):
+    high_adjusted = 224 // width
+    scaled_image = np.zeros((3, high_adjusted, high_adjusted))
+    return scaled_image
 
 
-def draw_random_line(im):
-    """Takes a 3x224x224 image and adds to it a random oriented line"""
+def scale_array_by_width(img, width):
+    scaled_image_red = np.repeat(np.repeat(img[0], width, axis=0), width, axis=1)
+    scaled_image_green = np.repeat(np.repeat(img[1], width, axis=0), width, axis=1)
+    scaled_image_blue = np.repeat(np.repeat(img[2], width, axis=0), width, axis=1)
 
-    r0 = np.random.randint(0, high=224)
-    c0 = np.random.randint(0, high=224)
+    scaled_image = np.stack((scaled_image_red, scaled_image_green, scaled_image_blue))
 
-    r1 = np.random.randint(0, high=224)
-    c1 = np.random.randint(0, high=224)
+    tuple_shape = scaled_image.shape
 
-    rr, cc = line(r0, c0, r1, c1)
+    print("Prior shape: ", tuple_shape)
 
-    im[:, rr,cc] = 255
+    number_padding_row = 224 - tuple_shape[1]
+    number_padding_column = 224 - tuple_shape[2]
 
-    return im
+    print(number_padding_row, number_padding_column)
+
+    scaled_adjusted_image_red = np.pad(scaled_image[0], ((0, number_padding_row),
+                                                         (0, number_padding_column)), mode='constant')
+    scaled_adjusted_image_green = np.pad(scaled_image[1], ((0, number_padding_row),
+                                                         (0, number_padding_column)), mode='constant')
+    scaled_adjusted_image_blue = np.pad(scaled_image[2], ((0, number_padding_row),
+                                                         (0, number_padding_column)), mode='constant')
+
+    scaled_adjusted_image = np.stack((scaled_adjusted_image_red, scaled_adjusted_image_green,
+                                                     scaled_adjusted_image_blue))
+
+    print("Post shape: ", scaled_adjusted_image.shape)
+
+    return scaled_adjusted_image
 
 
 
 def draw_random_line_aa(im, red, green, blue):
 
-    r0 = np.random.randint(0, high=224)
-    c0 = np.random.randint(0, high=224)
+    shape = im.shape
+    row_length = shape[1]
+    column_length = shape[2]
 
-    r1 = np.random.randint(0, high=224)
-    c1 = np.random.randint(0, high=224)
+    r0 = np.random.randint(0, high=row_length)
+    c0 = np.random.randint(0, high=column_length)
+
+    r1 = np.random.randint(0, high=row_length)
+    c1 = np.random.randint(0, high=column_length)
 
     rr, cc, val = line_aa(r0, c0, r1, c1)
 
@@ -57,11 +71,16 @@ def draw_random_line_aa(im, red, green, blue):
 
 def draw_random_color_line(im, red, green, blue):
 
-    r0 = np.random.randint(0, high=224)
-    c0 = np.random.randint(0, high=224)
+    shape = im.shape
+    row_length = shape[1]
+    column_length = shape[2]
 
-    r1 = np.random.randint(0, high=224)
-    c1 = np.random.randint(0, high=224)
+
+    r0 = np.random.randint(0, high=row_length)
+    c0 = np.random.randint(0, high=column_length)
+
+    r1 = np.random.randint(0, high=row_length)
+    c1 = np.random.randint(0, high=column_length)
 
     rr, cc = line(r0, c0, r1, c1)
 
@@ -70,6 +89,164 @@ def draw_random_color_line(im, red, green, blue):
     im[2, rr, cc] = blue
 
     return im
+
+
+def check_outbounds(value):
+    if (value < 0):
+        return 0
+    elif (value > 223):
+        return 223
+
+    return value
+
+
+"""
+This method draws a colored line perimeter give on a width. We compute two points randomly. Then, we take
+the negative inverse slope and width distance to find two prime points relative to the first two points. Finally, we
+use skimage draw line_aa four times with the four points to get a perimeter.
+
+Important to note***
+We return an array called "fake_img" that was previously empty so that we can easily fill in the perimeter if wanted 
+(if there were multiple lines, the fill command becomes much more complicated). 
+"""
+def draw_colored_line_perimeter(im, red, green, blue, width):
+
+    shape = im.shape
+    row_length = shape[1]
+    column_length = shape[2]
+
+    r0 = np.random.randint(0, high=row_length)
+    c0 = np.random.randint(0, high=column_length)
+
+    r1 = np.random.randint(0, high=row_length)
+    c1 = np.random.randint(0, high=column_length)
+
+    c0_prime = None
+    c1_prime = None
+
+    r0_prime = None
+    r1_prime = None
+
+    if ((c0 - c1) == 0):
+        #vertical line
+        c0_prime = c0 + width
+        c1_prime = c1+ width
+
+        r0_prime = r0
+        r1_prime = r1
+
+    elif ((r0 - r1) == 0):
+        #horizontal line
+
+        c0_prime = c0
+        c1_prime = c1
+
+        r0_prime = r0 + width
+        r1_prime = r1 + width
+
+    else:
+
+        m_slope = (r0 - r1) / (c0 - c1)
+
+        c0_prime = math.floor((c0 * m_slope ** 2 + c0 - math.sqrt((m_slope ** 4 + m_slope ** 2) * width ** 2)) /
+                              (m_slope ** 2 + 1))
+        c1_prime = math.floor((c1 * m_slope ** 2 + c1 - math.sqrt((m_slope ** 4 + m_slope ** 2) * width ** 2)) /
+                              (m_slope ** 2 + 1))
+
+        r0_prime = math.floor(((c0_prime - c0) / m_slope) + r0)
+        r1_prime = math.floor(((c1_prime - c1) / m_slope) + r1)
+
+    c0_prime = check_outbounds(c0_prime)
+    c1_prime = check_outbounds(c1_prime)
+
+    r0_prime = check_outbounds(r0_prime)
+    r1_prime = check_outbounds(r1_prime)
+
+
+    rr11, cc11, val11 = line_aa(r0, c0, r1, c1)
+    rr22, cc22, val22 = line_aa(r1, c1, r1_prime, c1_prime)
+    rr33, cc33, val33 = line_aa(r1_prime, c1_prime, r0_prime, c0_prime)
+    rr44, cc44, val44 = line_aa(r0_prime, c0_prime, r0, c0)
+
+    #line 1
+    im[0, rr11, cc11] = red * val11
+    im[1, rr11, cc11] = green * val11
+    im[2, rr11, cc11] = blue * val11
+    #line 2
+    im[0, rr22, cc22] = red * val22
+    im[1, rr22, cc22] = green * val22
+    im[2, rr22, cc22] = blue * val22
+    #line3
+    im[0, rr33, cc33] = red * val33
+    im[1, rr33, cc33] = green * val33
+    im[2, rr33, cc33] = blue * val33
+    #line 4
+    im[0, rr44, cc44] = red * val44
+    im[1, rr44, cc44] = green * val44
+    im[2, rr44, cc44] = blue * val44
+
+
+    ###Fill Fake image up. Needed to compute perimeter fill
+    fake_img = np.zeros((3, row_length, column_length))
+
+    #line 1
+    fake_img[0, rr11, cc11] = red * val11
+    fake_img[1, rr11, cc11] = green * val11
+    fake_img[2, rr11, cc11] = blue * val11
+    #line 2
+    fake_img[0, rr22, cc22] = red * val22
+    fake_img[1, rr22, cc22] = green * val22
+    fake_img[2, rr22, cc22] = blue * val22
+    #line3
+    fake_img[0, rr33, cc33] = red * val33
+    fake_img[1, rr33, cc33] = green * val33
+    fake_img[2, rr33, cc33] = blue * val33
+    #line 4
+    fake_img[0, rr44, cc44] = red * val44
+    fake_img[1, rr44, cc44] = green * val44
+    fake_img[2, rr44, cc44] = blue * val44
+
+    return fake_img
+
+"""
+Fill perimeter takes in an array representing an image. Then, finds all the columns per a row that have nonzero values.
+Thus, if we take a perimeter of a line, we know to fill in all the points in between with a color.
+"""
+def fill_perimeter(img):
+    dimension_tuple = img.shape
+    num_rows = dimension_tuple[1]
+    num_columns = dimension_tuple[2]
+    list_of_list= []
+    for row in range(num_rows):
+        count = 0
+        bool_color = False
+        list_x = []
+        for column in range(1, num_columns):
+            if (img[0][row][column] != 0):
+                list_x.append(column)
+
+        list_of_list.append(list_x)
+
+    return list_of_list
+
+
+def draw_color_line_width(im, red, green, blue, width):
+    fake_img = draw_colored_line_perimeter(im, red, green, blue, width)
+    list_of_list = fill_perimeter(fake_img)
+
+    for index in range(len(list_of_list)):
+        list_x = list_of_list[index]
+        if (len(list_x) > 1):
+            start = list_x[0]
+            stop = list_x[len(list_x) - 1]
+            im[0][index][start:stop] = red
+            im[1][index][start:stop] = green
+            im[2][index][start:stop] = blue
+
+
+
+
+
 
 def red_line_with_blue_distractors(filts, num_distractors = 10):
     """Routine for creating input and target images containing a straight red oriented line and with
@@ -90,14 +267,18 @@ def red_line_with_blue_distractors(filts, num_distractors = 10):
 
 def draw_bezier_curve_line(im, red, green, blue):
 
-    r0 = np.random.randint(0, high=224)
-    c0 = np.random.randint(0, high=224)
+    shape = im.shape
+    row_length = shape[1]
+    column_length = shape[2]
 
-    r1 = np.random.randint(0, high=224)
-    c1 = np.random.randint(0, high=224)
+    r0 = np.random.randint(0, high=row_length)
+    c0 = np.random.randint(0, high=column_length)
 
-    r2 = np.random.randint(0, high=224)
-    c2 = np.random.randint(0, high=224)
+    r1 = np.random.randint(0, high=row_length)
+    c1 = np.random.randint(0, high=column_length)
+
+    r2 = np.random.randint(0, high=row_length)
+    c2 = np.random.randint(0, high=column_length)
 
     weight = np.random.randint(0, 11)
 
@@ -135,9 +316,13 @@ def draw_random_polygon_perimeter(im, red, green, blue, n_sides):
     x_array = []
     y_array = []
 
+    shape = im.shape
+    row_length = shape[1]
+    column_length = shape[2]
+
     for i in range(n_sides):
-        x = np.random.randint(0, high=224)
-        y = np.random.randint(0, high=224)
+        x = np.random.randint(0, high=row_length)
+        y = np.random.randint(0, high=column_length)
 
         x_array.append(x)
         y_array.append(y)
@@ -164,6 +349,25 @@ def draw_random_ellipse_solid(im, red, green, blue):
         im[0, rr, cc] = red
         im[1, rr, cc] = green
         im[2, rr, cc] = blue
+
+
+def draw_random_ellipse_perimeter_thick(im, red, green, blue, width):
+    r = np.random.randint(0, high=224)
+    c = np.random.randint(0, high=224)
+
+    r_radius = np.random.uniform(0, 112)
+    c_radius = np.random.uniform(0, 112)
+
+    rotation = np.random.uniform(-np.pi, np.pi)
+
+    rr, cc = ellipse(r, c, r_radius, c_radius, shape=im[0].shape, rotation=rotation)
+
+    rr_small, cc_small = ellipse(r, c, r_radius - width, c_radius - width, shape = im[0].shape, rotation=rotation)
+    im[0, rr, cc] = red
+    im[1, rr, cc] = green
+    im[2, rr, cc] = blue
+
+    im[:, rr_small, cc_small] = 0
 
 
 def draw_random_ellipse_perimeter(im, red, green, blue):
@@ -293,11 +497,21 @@ def add_random_line_color_n(im, red, green, blue, n):
 
     return im
 
+
 def add_random_line_aa_color_n(im, red, green, blue, n):
     for x in range(n):
         draw_random_line_aa(im, red, green, blue)
 
     return im
+
+
+def add_random_line_aa_color_width_n(im, red, green, blue, max_width, n):
+    for x in range(n):
+        width = np.random.randint(0, high=max_width + 1)
+        draw_color_line_width(im, red, green, blue, width)
+
+    return im
+
 
 def add_random_polygon_perimeter_n(im, red, green, blue, n):
     for x in range(n):
@@ -305,43 +519,27 @@ def add_random_polygon_perimeter_n(im, red, green, blue, n):
 
     return im
 
-def add_random_ellipse_perimeter(im, red, green, blue, n):
+
+def add_random_ellipse_perimeter(im, red, green, blue, max_width, n):
     for x in range(n):
-        draw_random_ellipse_perimeter(im, red, green, blue)
+        width= np.random.randint(0, high=max_width + 1)
+        draw_random_ellipse_perimeter_thick(im, red, green, blue, width)
+
+    return im
+
 
 def add_random_curved_lines(im, red, green, blue, n):
     for x in range(n):
         draw_bezier_curve_line(im, red, green, blue)
 
+    return im
+
+
+
+
 def main():
     empty_img = np.zeros((3, 224, 224))
-
-    # print(empty_img)
-
-    # add_random_line(empty_img)
-
-    # bezier_curve_line(empty_img, 244, 244, 244)
-    # add_random_line_color_n(empty_img, 255, 255, 1, 100)
-
-    # add_test_line(empty_img)
-
-    print(empty_img.shape)
-    #
-    # add_random_line_aa(empty_img)
-    # random_polygon(empty_img, 200, 100, 55, 4)
-
-    # for row in range(244):
-    #     for col in range(244):
-    #         value = empty_img[0, row, col]
-    #         if (value != 0):
-    #             print (row, col)
-
-    draw_random_ellipse_perimeter(empty_img, 200, 100, 55)
-
-    KERNEL_SIZE = 15
-    filts = get_quadratures(KERNEL_SIZE)
-
-    empty_img = get_orientation_map(empty_img[0], filts)
+    draw_color_line_width(empty_img, 100, 212, 10, 25)
 
     plt.imshow(empty_img[0])
     plt.show()
--- from_lines/gen_and_save_lines.py
+++ from_lines/gen_and_save_lines.py
@@ -1,7 +1,7 @@
 from data_generators import (single_black_line, get_quadratures,
                              get_orientation_map, add_random_line_color_n, add_random_line_aa_color_n,
-                             add_random_polygon_perimeter_n, add_random_ellipse_perimeter, add_random_curved_lines
-
+                             add_random_polygon_perimeter_n, add_random_ellipse_perimeter, add_random_curved_lines,
+                             add_random_line_aa_color_width_n
                              )
 import pandas as pd
 import numpy as np
@@ -43,6 +43,22 @@ def gen_batch_thin_lines_aa(red, green, blue, num_lines_per_img):
     return all_inputs, all_targets
 
 
+def gen_batch_lines_aa_width(red, green, blue, max_width,num_lines_per_img):
+    all_inputs = list()
+    all_targets = list()
+    n_samples = 10000
+    for n in range(n_samples):
+        image_array = np.zeros((3, 224, 224))
+        input = add_random_line_aa_color_width_n(image_array, red, green, blue, max_width, num_lines_per_img)
+
+        target = get_orientation_map(input[0], filts)
+
+        all_inputs.append(input.reshape(-1))
+        all_targets.append(target.reshape(-1))
+
+    return all_inputs, all_targets
+
+
 def gen_batch_random_polygon_perimeter(red, green, blue, num_lines_per_img):
     all_inputs = list()
     all_targets = list()
@@ -59,13 +75,13 @@ def gen_batch_random_polygon_perimeter(red, green, blue, num_lines_per_img):
     return all_inputs, all_targets
 
 
-def gen_batch_random_ellipse_perimeter(red, green, blue, num_lines_per_img):
+def gen_batch_random_ellipse_perimeter(red, green, blue, max_width, num_lines_per_img):
     all_inputs = list()
     all_targets = list()
     n_samples = 10000
     for n in range(n_samples):
         image_array = np.zeros((3, 224, 224))
-        input = add_random_ellipse_perimeter(image_array, red, green, blue, num_lines_per_img)
+        input = add_random_ellipse_perimeter(image_array, red, green, blue, max_width, num_lines_per_img)
 
         target = get_orientation_map(input[0], filts)
 
@@ -74,7 +90,8 @@ def gen_batch_random_ellipse_perimeter(red, green, blue, num_lines_per_img):
 
     return all_inputs, all_targets
 
-def gen_batch_random_ellipse_perimeter(red, green, blue, num_lines_per_img):
+
+def gen_batch_random_curved_lines(red, green, blue, num_lines_per_img):
     all_inputs = list()
     all_targets = list()
     n_samples = 10000
@@ -90,12 +107,14 @@ def gen_batch_random_ellipse_perimeter(red, green, blue, num_lines_per_img):
     return all_inputs, all_targets
 
 
+
+
 def main():
 
     #add what is needed
 
     all_inputs = pd.DataFrame(np.stack(all_inputs))
-    all_inputs.to_hdf('/home/abenjamin/DNN_illusions/data/features/straight_lines/lines.h5', key="l", mode='w')
+    all_inputs.to_hdf('/home/rguan/DNN_illusions/data/features/straight_lines/lines.h5', key="l", mode='w')
 
     all_targets = pd.DataFrame(np.stack(all_targets))
     all_targets.to_hdf('/home/abenjamin/DNN_illusions/data/features/straight_lines/lines_targets.h5', key="l", mode='w')

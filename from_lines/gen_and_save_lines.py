f5a6e13fb07a9b838ced2c3f6c9b4d9dc1838a78
Ryan Guan <guan.ryan@gmail.com>
2019-02-10 19:08:31 -0500

    ellipse width comments

--- from_lines/gen_and_save_lines.py
+++ from_lines/gen_and_save_lines.py
@@ -111,35 +111,35 @@ def gen_batch_random_curved_lines(red, green, blue, num_lines_per_img):
 
 def main():
 
-    #thin_lines_aa first
-
-    all_inputs, all_targets = gen_batch_thin_lines_aa(255, 255, 255, 5)
-
-    all_inputs = pd.DataFrame(np.stack(all_inputs))
-    all_inputs.to_hdf('/home/rguan/DNN_illusions/data/geometric_array/five_thin_lines_input.h5', key="l", mode='w')
-
-    all_targets = pd.DataFrame(np.stack(all_targets))
-    all_targets.to_hdf('/home/rguan/DNN_illusions/data/geometric_array/five_thin_lines_targets.h5', key="l", mode='w')
-
-    #curved lines
-    all_inputs, all_targets = gen_batch_random_curved_lines(255, 255, 255, 10)
-
-    all_inputs = pd.DataFrame(np.stack(all_inputs))
-    all_inputs.to_hdf('/home/rguan/DNN_illusions/data/geometric_array/ten_thin_curved_lines_input.h5', key="l", mode='w')
-
-    all_targets = pd.DataFrame(np.stack(all_targets))
-    all_targets.to_hdf('/home/rguan/DNN_illusions/data/geometric_array/ten_thin_curved_lines_targets.h5', key="l", mode='w')
-
-
-    #thick lines aa
-
-    all_inputs, all_targets = gen_batch_lines_aa_width(255, 255, 255, 10, 5)
-
-    all_inputs = pd.DataFrame(np.stack(all_inputs))
-    all_inputs.to_hdf('/home/rguan/DNN_illusions/data/geometric_array/five_thick_lines_input.h5', key="l", mode='w')
-
-    all_targets = pd.DataFrame(np.stack(all_targets))
-    all_targets.to_hdf('/home/rguan/DNN_illusions/data/geometric_array/five_thick_lines_targets.h5', key="l", mode='w')
+    # #thin_lines_aa first
+    #
+    # all_inputs, all_targets = gen_batch_thin_lines_aa(255, 255, 255, 5)
+    #
+    # all_inputs = pd.DataFrame(np.stack(all_inputs))
+    # all_inputs.to_hdf('/home/rguan/DNN_illusions/data/geometric_array/five_thin_lines_input.h5', key="l", mode='w')
+    #
+    # all_targets = pd.DataFrame(np.stack(all_targets))
+    # all_targets.to_hdf('/home/rguan/DNN_illusions/data/geometric_array/five_thin_lines_targets.h5', key="l", mode='w')
+    #
+    # #curved lines
+    # all_inputs, all_targets = gen_batch_random_curved_lines(255, 255, 255, 10)
+    #
+    # all_inputs = pd.DataFrame(np.stack(all_inputs))
+    # all_inputs.to_hdf('/home/rguan/DNN_illusions/data/geometric_array/ten_thin_curved_lines_input.h5', key="l", mode='w')
+    #
+    # all_targets = pd.DataFrame(np.stack(all_targets))
+    # all_targets.to_hdf('/home/rguan/DNN_illusions/data/geometric_array/ten_thin_curved_lines_targets.h5', key="l", mode='w')
+    #
+    #
+    # #thick lines aa
+    #
+    # all_inputs, all_targets = gen_batch_lines_aa_width(255, 255, 255, 10, 5)
+    #
+    # all_inputs = pd.DataFrame(np.stack(all_inputs))
+    # all_inputs.to_hdf('/home/rguan/DNN_illusions/data/geometric_array/five_thick_lines_input.h5', key="l", mode='w')
+    #
+    # all_targets = pd.DataFrame(np.stack(all_targets))
+    # all_targets.to_hdf('/home/rguan/DNN_illusions/data/geometric_array/five_thick_lines_targets.h5', key="l", mode='w')
 
     #ellipses
 
